64606247b41d25c3ff1d80795ed229de781eef8a
Larry Jones <larry.jones@reveal-energy.com>
2022-07-10 15:27:49 -0500

    First program: Hello, World
    Previous commits used manual commands to compile code from a package
    on the network.
    
    This commit configures usage of `typescript` (installing it locally),
    initializes a compiler configuration file (`tsconfig.json`), and builds
    the "Hello World" project using this file.

--- hello-ts/index.ts
+++ hello-ts/index.ts
@@ -1,3 +1,5 @@
-let x;
-let y;
-const sum = x + y;
+let x: number = 7;
+let y: number = 11;
+const sum: number = x + y;
+const result: string = `The sum of ${x} and ${y} is ${sum}`
+console.log(result);
