fa968cd495ecc74797481d0d872dbf5bf4f30c83
Larry Jones <larry.jones@reveal-energy.com>
2022-07-11 19:12:52 -0500

    Return void

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,5 +1,11 @@
 const prefix = '🐉 ';
 
+type ProductType = {
+  id: number,
+  name: string,
+  icon?: string,
+};
+
 export default async function updateOutput(id: string) {
   // Note that the `id` parameter is **not** the product ID but the ID of the `div` in the page.
   // TODO
@@ -22,7 +28,7 @@ function runTheLearningSamples() {
   console.log(`${prefix} function declaration`);
   console.log(`${addNumbersDeclaration(7, 11)}`);
 
-  function addNumbersDeclaration(x: number, y: number) {
+  function addNumbersDeclaration(x: number, y: number): number {
     const sum: number = x + y;
     return sum;
   }
@@ -35,4 +41,44 @@ function runTheLearningSamples() {
 
   console.log(`${prefix} function expression`);
   console.log(`${addNumbersExpression(5, 8)}`);
+
+  const sampleProducts = [
+    {
+      id: 10,
+      name: 'Pizza slice',
+      icon: `fas fa-pizza-slice`
+    },
+    {
+      id: 20, name: 'Ice cream', icon: 'fas fa-ice-cream' },
+    {
+      id: 30,
+      name: 'Cheese',
+      icon: 'fas fa-cheese'
+    },
+  ];
+
+  function getProductNames() : string[] {
+    return sampleProducts.map((p)=> p.name);
+  }
+
+  console.log(`${prefix} return array`);
+  console.log(getProductNames());
+
+  function getProductById(id: number) : ProductType | undefined {
+    return sampleProducts.find(p => p.id === id);
+  }
+
+  console.log(`${prefix} ProductType`);
+  console.log(getProductById(10));
+  console.log(getProductById(21));
+
+  function displayProducts(products: ProductType[]) : void{
+    const productNames = products.map(p => p.name.toLowerCase());
+    const msg = `Sample products include ${productNames.join(', ')}`;
+
+    console.log(`${prefix} return void`);
+    console.log(`${msg}`);
+  }
+
+  displayProducts(sampleProducts);
 }
