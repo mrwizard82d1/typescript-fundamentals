0dd42fc5817941e13d22b5a01e0ccd9657c57d6f
Larry Jones <larry.jones@reveal-energy.com>
2022-07-18 17:47:57 -0500

    Creating a class (application BROKE)

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,15 +1,7 @@
-import { Product } from './interfaces';
-
-// This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
-abstract class ProductBase implements Product {
-  constructor(public id: number, public name: string, public icon: string) {}
-  validate(): boolean {
-    throw new Error('Not implemented');
-  }
+class Product {
+    // properties / fields
+    // constructor
+    // functions
 }
 
-export class FoodProduct extends ProductBase {
-  validate(): boolean {
-    return !!this.id && !!this.name && !!this.icon;
-  }
-}
+let product: Product = new Product();
