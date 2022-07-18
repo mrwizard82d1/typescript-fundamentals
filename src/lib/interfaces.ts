ad06f5db88e2e39e7193a4f76e07a5d3c28cd802
Larry Jones <larry.jones@reveal-energy.com>
2022-07-18 17:13:10 -0500

    Define an interface (with optional members)

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,29 +1,6 @@
 export interface Product {
-  id: number;
-  name: string;
-  icon: string;
-  description?: string;
-  validate(): boolean;
+    id: number,
+    name: string,
+    icon: string,
+    description? : string,
 }
-
-// Examples of using a type alias
-type ProductAlias =
-  | string
-  | number
-  | {
-      id: number;
-      name: string;
-      icon: string;
-      description?: string;
-    };
-
-let product: ProductAlias = 'Food';
-
-// Using a type alias versus an enum
-enum ProductType {
-  Sporting,
-  Home,
-}
-
-type ProductTypeList = 'SPORTING' | 'HOME';
-let p: ProductTypeList = 'SPORTING';
