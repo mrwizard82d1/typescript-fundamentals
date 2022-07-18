6203d825e0528478d77292d8af56aad0ba6ab0a4
Larry Jones <larry.jones@reveal-energy.com>
2022-07-18 17:15:13 -0500

    Create an object implementing an interface

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -4,3 +4,10 @@ export interface Product {
     icon: string,
     description? : string,
 }
+
+let product: Product = {
+    id: 12,
+    name: 'SomeProduct',
+    icon: 'some_pic.jpg',
+    description: 'some_description'
+}
